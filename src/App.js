91059f8de2b3b7bd7a86f018340caaace6da1f25
Alan Sánchez <alan247@gmail.com>
2017-09-04 18:41:35 -0500

    Create List and Search components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {Route} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
+import BookList from './components/BookList'
+import BookSearch from './components/BookSearch'
 
-class BooksApp extends React.Component {
-  // state = {
-  //   *
-  //    * TODO: Instead of using this state variable to keep track of which page
-  //    * we're on, use the URL in the browser's address bar. This will ensure that
-  //    * users can use the browser's back and forward buttons to navigate between
-  //    * pages, as well as provide a good URL they can bookmark and share.
 
-  //   showSearchPage: false
-  // }
-
+class BooksApp extends Component {
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <div> Main </div>
+          <BookList/>
         )}/>
         <Route exact path="/search" render={() => (
-          <div> Search </div>
+          <BookSearch/>
         )}/>
       </div>
     )
