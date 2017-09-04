9924b14b6821c66290e87b9f96fde681eb673fe0
Alan Sánchez <alan247@gmail.com>
2017-09-04 18:45:44 -0500

    Add Router <Link> tags

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 
 class BookList extends Component {
   render() {
     return(
+
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
@@ -160,7 +162,9 @@ class BookList extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link
+            to="/search"
+          >Add a book</Link>
         </div>
       </div>
     )
