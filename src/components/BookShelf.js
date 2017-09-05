9b414646eeddff434646733c174977519cdfcbb1
Alan Sánchez <alan247@gmail.com>
2017-09-04 19:52:49 -0500

    Add BookItem & BookShelf components

---
+++ src/components/BookShelf.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import BookItem from './BookItem'
+
+class BookShelf extends Component {
+  render() {
+    return(
+			<div className="bookshelf">
+	      <h2 className="bookshelf-title">Currently Reading</h2>
+	      <div className="bookshelf-books">
+	        <ol className="books-grid">
+	          <li>
+	            <BookItem />
+	          </li>
+	        </ol>
+	      </div>
+	    </div>
+   	)
+  }
+}
+
+export default BookShelf
